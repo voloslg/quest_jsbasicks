1f0320245b1ee6403d9633548000ed91f04bc253
andrey <volosgoto@yahoo.com>
2019-04-06 14:56:23 +0300

    task01 alt solution

--- task01/app.js
+++ task01/app.js
@@ -1,15 +1,24 @@
-
 let num = -2;
 
-
 let palmTree = (num) => {
-  if (num > 0) {
-    let str = '';
-    for (let i = 1; i <= num; i++) {
-      str += 'p';
+  if (typeof num === 'number') {
+    if (num > 0) {
+      let str = '';
+      for (let i = 1; i <= num; i++) {
+        str += 'p';
+      }
+      return str;
     }
-    return str;
   }
   return false;
 }
-console.log(palmTree(num)); 
+console.log(palmTree(5));
+
+
+// Alternate solution
+palmTree = (num) => {
+  return (typeof num === 'number' && num > 0) ? 'p'.repeat(num) : false;
+}
+console.log(palmTree(3));
+
+
